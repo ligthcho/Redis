4e1cc54beaaa2450ce5919d7b4e1620bedd678a7
lightcho <853358540@qq.com>
2018-08-10 15:16:01 +0800

    添加项目文件。

---
+++ RedisTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RedisTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
